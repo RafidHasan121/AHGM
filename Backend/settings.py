d14b91efa9678d592d4a1e1e1fb2751e872532ad
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2024-08-11 01:50:18 +0400

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['ahgm-production.up.railway.app', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://ahgm-production.up.railway.app','https://127.0.0.1']
+
 
 # Application definition
 
