d23ef9e1e184d7deafa8a22a04d46a4817afd1de
RafidHasan121 <Rafidhasan121@gmail.com>
2024-09-01 00:08:45 +0600

    refactor: Update EmployeeViewSet destroy method to delete employee and return serialized data

--- UserApp/views.py
+++ UserApp/views.py
@@ -64,7 +64,12 @@ class EmployeeViewSet(ModelViewSet):
 
         return super().update(request, *args, **kwargs)
 
-
+    def destroy(self, request, *args, **kwargs):
+        instance = self.queryset.get(user=kwargs['pk'])
+        self.perform_destroy(instance)
+        serializer = EmployeeSerializer(instance)
+        return Response(serializer.data, status=200)
+    
 class projectViewSet(ModelViewSet):
     queryset = project.objects.all()
     serializer_class = ProjectSerializer
