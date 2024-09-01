9d1434fa3e6db1f0270743938d1521a164fb172c
RafidHasan121 <Rafidhasan121@gmail.com>
2024-09-02 01:41:08 +0600

    refactor: Update EmployeeViewSet update method to handle partial updates

--- UserApp/views.py
+++ UserApp/views.py
@@ -63,6 +63,20 @@ class EmployeeViewSet(ModelViewSet):
     #             self.get_object().user.save()
 
     #     return super().update(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs): 
+        partial = kwargs.pop('partial', False)
+        instance = self.queryset.get(user=kwargs['pk'])
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.queryset.get(user=kwargs['pk'])
